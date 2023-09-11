9ae129c73abdc080ea84f24b4299af2e33a022f8
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2023-09-11 11:07:07 +0530

    created new branch navbar

--- client/src/Components/Components/HomeComponents/About/About.jsx
+++ client/src/Components/Components/HomeComponents/About/About.jsx
@@ -5,19 +5,19 @@ const About = () => {
     const body = "The Students' Gymkhana is the students' government organization of The LNMIIT, Jaipur. Established in 2006, the Gymkhana seeks to cultivate qualities of leadership amongst students to foster a corporate life and to advance the general cause of the student body of the student body of the Institute. It aims at promoting and developing organizational abilities in students. It has been successful over the years in evolving a well-informed, articulate and participatory student community life. It has been instrumental in identifying student issues and promoting discussion on them. It functions as the office for all election and nominations of students for gymkhana activities. The Students' Gymkhana functions mainly through the Students' Senate, an elected representative body and the elected executives: the President, General Secretary Cultural Council, General Secretary Science and Technology Council, General Secretary Games and Sports Council. The Senate lays down the general guideline for the functions of the executives and their associated councils. The Senators elect their own Senate Convener."
 
 
-  return (
-    <div className={styles.about}>
-        <h2 className={styles.heading}>About Us</h2>
-        <div className={styles.body}>
-            <div className={text_box}>
-                {body}
-            </div>
-            <div className={styles.image}>
-                <img src="./gymkhana-logo.png" alt="" />
+    return (
+        <div className={styles.about}>
+            <h2 className={styles.heading}>About Us</h2>
+            <div className={styles.body}>
+                <div className={styles.text_box}>
+                    {body}
+                </div>
+                <div className={styles.image}>
+                    <img src="./gymkhana-logo.png" alt="" />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 };
 
 export default About;
--- client/src/Components/Components/HomeComponents/HeroSection/HeroSection.jsx
+++ client/src/Components/Components/HomeComponents/HeroSection/HeroSection.jsx
@@ -3,7 +3,10 @@ import styles from './HeroSection.module.css';
 
 const HeroSection = () => {
   return (
-    <div className={{width: "90vh"}}>HeroSection</div>
+        <div className={styles.outer}>
+            <h1>Introducing,<br /> The LNMIIT Student's Gymkhana</h1>
+            <h2>Welcome to the Official Website.</h2>
+        </div>
   )
 };
 
